2032b1a1a8d84192ec7b63a1a1ba4f92ed31a5f1
Edris <edrisadel64@gmail.com>
2026-03-27 22:46:48 -0400

    Fix GitHub Pages Firebase config import

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4CPP5pCtgcr_fpxt74lp8Swf2xlqGYls",
+  authDomain: "cp317-ecom.firebaseapp.com",
+  projectId: "cp317-ecom",
+  storageBucket: "cp317-ecom.firebasestorage.app",
+  messagingSenderId: "777820777619",
+  appId: "1:777820777619:web:914f9000a5ca0015c8f166"
+};
--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
-import { firebaseConfig } from "./firebase-config.local.js";
+import { firebaseConfig } from "./firebase-config.js";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
